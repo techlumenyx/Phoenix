211ba213c695666873bf62854a9d0058a742379f
rohangoswami1039 <rohangoswami1039@gmail.com>
2026-06-22 04:21:12 +0530

    fix: quote path in nx/nx.bat wrappers to support directories with spaces

--- nx.bat
+++ nx.bat
@@ -5,6 +5,6 @@ WHERE node >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (ECHO Nx requires NodeJS to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download/ . & GOTO exit)
 WHERE npm >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (ECHO Nx requires npm to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download/ . & GOTO exit)
-node %path_to_root%\.nx\nxw.js %*
+node "%path_to_root%.nx\nxw.js" %*
 :exit
   cmd /c exit /b %ERRORLEVEL%
