8d6495c0704eb3f17b883d35e7c3e25e2f1278e4
rohangoswami1039 <rohangoswami1039@gmail.com>
2026-06-22 04:23:59 +0530

    fix: point nx/nx.bat wrappers to local node_modules instead of nxw.js

--- nx.bat
+++ nx.bat
@@ -3,8 +3,6 @@ SETLOCAL
 SET path_to_root=%~dp0
 WHERE node >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (ECHO Nx requires NodeJS to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download/ . & GOTO exit)
-WHERE npm >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 (ECHO Nx requires npm to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download/ . & GOTO exit)
-node "%path_to_root%.nx\nxw.js" %*
+node "%path_to_root%node_modules\nx\dist\bin\nx.js" %*
 :exit
   cmd /c exit /b %ERRORLEVEL%
